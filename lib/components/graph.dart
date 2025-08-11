23a4f511d6fe1ce4796e05e8375c862933cc3954
kao126 <76607249+kao126@users.noreply.github.com>
2025-08-11 13:06:35 +0900

    update: graph page

--- lib/components/graph.dart
+++ lib/components/graph.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
+import 'package:nantokanaru/db/database_helper.dart';
+import 'package:nantokanaru/models/trade_record.dart';
 
 class GraphPage extends StatefulWidget {
   const GraphPage({super.key});
@@ -10,64 +13,111 @@ class GraphPage extends StatefulWidget {
 }
 
 class _GraphPageState extends State<GraphPage> {
-  final TextEditingController _startYearController = TextEditingController();
-  final TextEditingController _startMonthController = TextEditingController();
-  final TextEditingController _endYearController = TextEditingController();
-  final TextEditingController _endMonthController = TextEditingController();
+  late List<TradeRecord> _tradeRecords;
+  final TextEditingController _yearController = TextEditingController();
+  final TextEditingController _monthController = TextEditingController();
   final List<int> _years =
       List.generate(DateTime.now().year - 2000 + 1, (index) => 2000 + index);
   final List<int> _months = List.generate(12, (index) => index + 1);
-  int _selectedStartYear = DateTime.now().year;
-  int _selectedStartMonth = DateTime.now().month;
-  int _selectedEndYear = DateTime.now().year;
-  int _selectedEndMonth = DateTime.now().month;
-
-  final _datas = <int, List<double>>{
-    DateTime(2025, 8, 1).day: [1, 2, 3, 4],
-    DateTime(2025, 8, 2).day: [-1, -2, -3, -4],
-    DateTime(2025, 8, 3).day: [1.5, 2, 3.5, 6],
-    DateTime(2025, 8, 4).day: [1.5, 1.5, 4, 6.5],
-    DateTime(2025, 8, 5).day: [-2, -2, -5, -9],
-    DateTime(2025, 8, 6).day: [-1.2, -1.5, -4.3, -10],
-    DateTime(2025, 8, 7).day: [1.2, 4.8, 5, 5],
-  };
+  final NumberFormat _yenFormat =
+      NumberFormat.currency(locale: 'ja_JP', symbol: '¥', decimalDigits: 0);
+  int _selectedYear = DateTime.now().year;
+  int _selectedMonth = DateTime.now().month;
 
   @override
   void initState() {
     super.initState();
-    _startYearController.text = "$_selectedStartYear";
-    _startMonthController.text = "$_selectedStartMonth";
-    _endYearController.text = "$_selectedEndYear";
-    _endMonthController.text = "$_selectedEndMonth";
+    _tradeRecords = [];
+    _fetchTradeRecords();
+    _yearController.text = "$_selectedYear";
+    _monthController.text = "$_selectedMonth";
   }
 
   @override
   void dispose() {
-    _startYearController.dispose();
-    _startMonthController.dispose();
-    _endYearController.dispose();
-    _endMonthController.dispose();
+    _yearController.dispose();
+    _monthController.dispose();
     super.dispose();
+  }
+
+  // データベースの内容をページに反映
+  Future<void> _fetchTradeRecords() async {
+    List<TradeRecord> tradeRecords = await DatabaseHelper.instance
+        .getMonthData(_selectedYear, _selectedMonth);
+    setState(() {
+      _tradeRecords = tradeRecords;
+    });
+  }
+
+  // データベースの内容を集計
+  Map<DateTime, List<double>> _aggregateRecords(
+      List<TradeRecord> tradeRecords) {
+    final Map<DateTime, List<double>> datas = {};
+
+    void initializeMonthData(int year, int month) {
+      final firstDay = DateTime(year, month, 1);
+      final lastDay = DateTime(year, month + 1, 0); // 月末日を正確に取得
+
+      for (var d = firstDay;
+          !d.isAfter(lastDay);
+          d = d.add(const Duration(days: 1))) {
+        datas[d] = [0.0]; // 初期値0.0でセット
+      }
+    }
+
+    initializeMonthData(_selectedYear, _selectedMonth);
+
+    // 選択された月の範囲内のデータのみを処理
+    for (var record in tradeRecords) {
+      final recordDate =
+          DateTime(record.date.year, record.date.month, record.date.day);
+      final selectedMonthStart = DateTime(_selectedYear, _selectedMonth, 1);
+      final selectedMonthEnd = DateTime(_selectedYear, _selectedMonth + 1, 0);
+
+      // 選択された月の範囲内かチェック
+      if (recordDate
+              .isAfter(selectedMonthStart.subtract(const Duration(days: 1))) &&
+          recordDate.isBefore(selectedMonthEnd.add(const Duration(days: 1)))) {
+        datas.putIfAbsent(recordDate, () => []);
+        datas[recordDate]!.add(record.profitLoss.toDouble());
+      }
+    }
+
+    final sortedEntries = datas.entries.toList()
+      ..sort((a, b) => a.key.compareTo(b.key));
+
+    return Map.fromEntries(sortedEntries);
+  }
+
+  List<DateTime> getDatesInMonth(int year, int month) {
+    final lastDay = DateTime(year, month + 1, 0).day;
+
+    List<DateTime> days = [];
+    for (int i = 1; i <= lastDay; i++) {
+      days.add(DateTime(year, month, i));
+    }
+    return days;
   }
 
   void _showDatePicker({
     required List<int> options,
-    required int selectedYear,
+    required int selectedValue,
     required void Function(int) onDateChanged,
-  }) {
-    showCupertinoModalPopup(
+  }) async {
+    int tempSelectedValue = selectedValue;
+    await showCupertinoModalPopup(
       context: context,
       builder: (_) => Container(
         height: 250,
         color: Colors.white,
         child: CupertinoPicker(
           scrollController: FixedExtentScrollController(
-            initialItem: options.indexOf(selectedYear),
+            initialItem: options.indexOf(selectedValue),
           ),
           itemExtent: 32.0,
           onSelectedItemChanged: (int index) {
             setState(() {
-              onDateChanged(options[index]);
+              tempSelectedValue = options[index];
             });
           },
           children:
@@ -75,14 +125,39 @@ class _GraphPageState extends State<GraphPage> {
         ),
       ),
     );
+    setState(() {
+      onDateChanged(tempSelectedValue);
+    });
+    _fetchTradeRecords();
   }
 
-  Widget bottomTitles(double date, TitleMeta meta) {
-    return SideTitleWidget(
-      axisSide: AxisSide.bottom,
-      child: Text('$_selectedStartMonth/${date.toInt()}',
-          style: const TextStyle(color: Colors.black, fontSize: 10)),
-    );
+  Widget bottomTitles(double value, TitleMeta meta) {
+    final dates = getDatesInMonth(_selectedYear, _selectedMonth);
+    final int index = value.toInt();
+
+    // インデックスの範囲チェック
+    if (index < 0 || index >= dates.length) {
+      return const SideTitleWidget(
+        axisSide: AxisSide.bottom,
+        child: Text(''),
+      );
+    }
+
+    DateTime date = dates[index];
+    if (index % 5 == 0 || index == 0) {
+      return SideTitleWidget(
+        axisSide: AxisSide.bottom,
+        child: Text(
+          index == 0 ? "${date.month}/${date.day}" : "${date.day}",
+          style: const TextStyle(color: Colors.black, fontSize: 10),
+        ),
+      );
+    } else {
+      return const SideTitleWidget(
+        axisSide: AxisSide.bottom,
+        child: Text(''),
+      );
+    }
   }
 
   Widget leftTitles(double value, TitleMeta meta) {
@@ -97,19 +172,14 @@ class _GraphPageState extends State<GraphPage> {
     );
   }
 
-  BarChartGroupData generateGroup(
-    int date,
-    double sum,
-  ) {
+  BarChartGroupData generateGroup(int date, double sum) {
     final isTop = sum > 0;
     return BarChartGroupData(
       x: date,
-      // groupVertically: true,
-      // showingTooltipIndicators: [],
       barRods: [
         BarChartRodData(
           toY: sum,
-          width: 22,
+          width: 10,
           color: Colors.amber,
           borderRadius: isTop
               ? const BorderRadius.only(
@@ -143,10 +213,9 @@ class _GraphPageState extends State<GraphPage> {
           children: [
             Row(
               children: [
-                Flexible(
-                  flex: 1,
+                Expanded(
                   child: TextFormField(
-                    controller: _startYearController,
+                    controller: _yearController,
                     textAlign: TextAlign.center,
                     readOnly: true,
                     decoration: const InputDecoration(
@@ -156,19 +225,20 @@ class _GraphPageState extends State<GraphPage> {
                     ),
                     onTap: () => _showDatePicker(
                       options: _years,
-                      selectedYear: _selectedStartYear,
+                      selectedValue: _selectedYear,
                       onDateChanged: (int newYear) {
-                        _selectedStartYear = newYear;
-                        _startYearController.text = "$newYear";
+                        setState(() {
+                          _selectedYear = newYear;
+                          _yearController.text = "$newYear";
+                        });
                       },
                     ),
                   ),
                 ),
                 const SizedBox(width: 8),
-                Flexible(
-                  flex: 1,
+                Expanded(
                   child: TextFormField(
-                    controller: _startMonthController,
+                    controller: _monthController,
                     textAlign: TextAlign.center,
                     readOnly: true,
                     decoration: const InputDecoration(
@@ -178,56 +248,12 @@ class _GraphPageState extends State<GraphPage> {
                     ),
                     onTap: () => _showDatePicker(
                       options: _months,
-                      selectedYear: _selectedStartMonth,
+                      selectedValue: _selectedMonth,
                       onDateChanged: (int newMonth) {
-                        _selectedStartMonth = newMonth;
-                        _startMonthController.text = "$newMonth";
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                const Text("~"),
-                const SizedBox(width: 8),
-                Flexible(
-                  flex: 1,
-                  child: TextFormField(
-                    controller: _endYearController,
-                    textAlign: TextAlign.center,
-                    readOnly: true,
-                    decoration: const InputDecoration(
-                      labelText: "年",
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(),
-                    ),
-                    onTap: () => _showDatePicker(
-                      options: _years,
-                      selectedYear: _selectedEndYear,
-                      onDateChanged: (int newYear) {
-                        _selectedEndYear = newYear;
-                        _endYearController.text = "$newYear";
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Flexible(
-                  flex: 1,
-                  child: TextFormField(
-                    controller: _endMonthController,
-                    textAlign: TextAlign.center,
-                    readOnly: true,
-                    decoration: const InputDecoration(
-                      labelText: "月",
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(),
-                    ),
-                    onTap: () => _showDatePicker(
-                      options: _months,
-                      selectedYear: _selectedEndMonth,
-                      onDateChanged: (int newMonth) {
-                        _selectedEndMonth = newMonth;
-                        _endMonthController.text = "$newMonth";
+                        setState(() {
+                          _selectedMonth = newMonth;
+                          _monthController.text = "$newMonth";
+                        });
                       },
                     ),
                   ),
@@ -249,7 +275,6 @@ class _GraphPageState extends State<GraphPage> {
                     bottomTitles: AxisTitles(
                       sideTitles: SideTitles(
                         showTitles: true,
-                        // reservedSize: 32,
                         getTitlesWidget: bottomTitles,
                       ),
                     ),
@@ -257,12 +282,30 @@ class _GraphPageState extends State<GraphPage> {
                       sideTitles: SideTitles(
                         showTitles: true,
                         getTitlesWidget: leftTitles,
-                        // interval: 5,
-                        // reservedSize: 42,
+                        reservedSize: 50,
                       ),
                     ),
                     rightTitles: const AxisTitles(
                       sideTitles: SideTitles(showTitles: false),
+                    ),
+                  ),
+                  barTouchData: BarTouchData(
+                    enabled: true,
+                    touchTooltipData: BarTouchTooltipData(
+                      tooltipPadding: const EdgeInsets.all(8.0),
+                      tooltipBgColor: Colors.grey[700],
+                      getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                        final dates =
+                            getDatesInMonth(_selectedYear, _selectedMonth);
+
+                        return BarTooltipItem(
+                          '${dates[groupIndex].month.toString()}/${dates[groupIndex].day.toString()}\n${_yenFormat.format(rod.toY.toInt())}',
+                          const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
                     ),
                   ),
                   gridData: FlGridData(
@@ -285,10 +328,12 @@ class _GraphPageState extends State<GraphPage> {
                   borderData: FlBorderData(
                     show: false,
                   ),
-                  barGroups: _datas.entries
+                  barGroups: _aggregateRecords(_tradeRecords)
+                      .entries
+                      .where((e) => e.value.isNotEmpty) // 空のデータを除外
                       .map(
                         (e) => generateGroup(
-                          e.key,
+                          e.key.day - 1, // インデックスを0から開始
                           e.value.reduce((a, b) => a + b),
                         ),
                       )
