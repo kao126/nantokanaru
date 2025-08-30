5a7dc5b2d8894587b8231573e13635383ceabe98
kao126 <76607249+kao126@users.noreply.github.com>
2025-08-30 23:47:47 +0900

    update: csv page

--- lib/components/csv.dart
+++ lib/components/csv.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:nantokanaru/utils/csv_import.dart';
-import 'package:nantokanaru/db/database_helper.dart';
+// import 'package:nantokanaru/db/database_helper.dart';
+import 'package:nantokanaru/utils/csv_loader.dart';
+import 'package:nantokanaru/utils/csv_format.dart';
 
 class CsvPage extends StatefulWidget {
   const CsvPage({super.key});
@@ -13,22 +15,53 @@ class CsvPage extends StatefulWidget {
 class _CsvPageState extends State<CsvPage> {
   late List<String> _tableColumns;
   CsvData _csv = CsvData(headers: [], rows: []);
+  List<String> _issueSymbolData = [];
 
   @override
   void initState() {
     super.initState();
-    _tableColumns = [];
-    _fetchDBColumns();
+    _tableColumns = ["日付", "種別", "銘柄名", "銘柄コード", "カテゴリ", "金額"];
+    _loadData();
+    // _fetchDBColumns();
   }
 
-  // データベースのカラムを取得
-  Future<void> _fetchDBColumns() async {
-    List<String> columns =
-        await DatabaseHelper.instance.getTableColumns("trade_records");
+  Future<void> _loadData() async {
+    final data = await loadCsvFromAssets('assets/data_j.csv');
+    final formattedData = formatNameAndCode(data);
     setState(() {
-      _tableColumns = columns;
+      _issueSymbolData = formattedData;
     });
   }
+
+  // 文字列から銘柄名とコードを分離
+  Set<String> _setIssueAndSymbol(String issue) {
+    final data = _issueSymbolData.firstWhere((option) => option.contains(issue),
+        orElse: () => '');
+    final regex = RegExp(r'^(.*)\((.*)\)$'); // "銘柄名(コード)"形式
+    RegExpMatch? match;
+    if (issue == "DeNA") {
+      match = regex.firstMatch("ディー・エヌ・エー(2432)");
+    } else {
+      match = regex.firstMatch(data);
+    }
+    if (match != null) {
+      final issue = match.group(1) ?? '';
+      final symbol = match.group(2) ?? '';
+      return {issue, symbol};
+    } else {
+      print(issue);
+      return {};
+    }
+  }
+
+  // // データベースのカラムを取得
+  // Future<void> _fetchDBColumns() async {
+  //   List<String> columns =
+  //       await DatabaseHelper.instance.getTableColumns("trade_records");
+  //   setState(() {
+  //     _tableColumns = columns;
+  //   });
+  // }
 
   Future<void> pickCsvFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -51,35 +84,38 @@ class _CsvPageState extends State<CsvPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => Navigator.of(context).pop(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
               ),
-            ),
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.import_export),
-                onPressed: () => pickCsvFile(),
+              ListTile(
+                leading: IconButton(
+                  icon: const Icon(Icons.import_export),
+                  onPressed: () => pickCsvFile(),
+                ),
               ),
-            ),
-            _csv.headers.isNotEmpty
-                ? Column(
-                    children: _csv.headers
-                        .map((header) => Center(child: Text(header)))
-                        .toList(),
-                  )
-                : const Center(child: Text("csv")),
-            _tableColumns.isNotEmpty
-                ? Column(
-                    children: _tableColumns
-                        .map((column) => Center(child: Text(column)))
-                        .toList(),
-                  )
-                : const Center(child: Text("column")),
-          ],
+              _csv.rows.isNotEmpty
+                  ? Column(
+                      children: _csv.rows.map((row) {
+                        final data = _setIssueAndSymbol(row[3]);
+                        return Center(child: Text(data.toString()));
+                      }).toList(),
+                    )
+                  : const Center(child: Text("csv")),
+              _tableColumns.isNotEmpty
+                  ? Column(
+                      children: _tableColumns
+                          .map((column) => Center(child: Text(column)))
+                          .toList(),
+                    )
+                  : const Center(child: Text("column")),
+            ],
+          ),
         ),
       ),
     );
